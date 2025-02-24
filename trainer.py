af78128068b15de3d1bb8dc1c51514315c6c6076
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2025-02-24 21:24:02 +0100

    Add empty trainer.py

---
+++ trainer.py
@@ -0,0 +1,5 @@
+# This script is intended to train writing text with the controller
+# It will show a text and highlight the buttons that need to be pressed
+# It will start with a small set of the most common letters and gradually increase the difficulty
+
+# TODO implement
