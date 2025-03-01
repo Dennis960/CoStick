c91bbc350ff99e5d2dece659269627ebd0b7f3ad
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2025-03-01 10:11:43 +0100

    Update dataset allowed chars

--- optimize_layout/load_dataset.py
+++ optimize_layout/load_dataset.py
@@ -6,7 +6,7 @@ path_to_script = os.path.dirname(os.path.abspath(__file__))
 
 
 DATASET_PATH = os.path.join(path_to_script, "dataset.txt")
-ALLOWED_CHARS = "1234567890ß!\"$%&/()=?'+#-.,*'_:;<>|{[]}\\~@€^`qwertzuiopüasdfghjklöäyxcvbnmQWERTZUIOPÜASDFGHJKLÖÄYXCVBNM \n\t"
+ALLOWED_CHARS = "1234567890ß!\"$%&/()=?'+#-.,*'_:;<>|{[]}\\~@€^`°qwertzuiopüasdfghjklöäyxcvbnmQWERTZUIOPÜASDFGHJKLÖÄYXCVBNM \n\t"
 IGNORED_CHARS = "€~^\t"
 NEEDED_CHARS = set(set(ALLOWED_CHARS.lower()) - set(IGNORED_CHARS))
 
@@ -51,7 +51,7 @@ with open(DATASET_PATH, "r", encoding="utf-8") as f:
 
 if __name__ == "__main__":
     chars = set(dataset)
-    my_chars = "1234567890ß!\"$%&/()=?'+#-.,*'_:;<>|{[]}\\~@€^`qwertzuiopüasdfghjklöäyxcvbnmQWERTZUIOPÜASDFGHJKLÖÄYXCVBNM \n"
+    my_chars = ALLOWED_CHARS.lower()
     print(f"Unique characters in dataset: {len(chars)}")
     print(f"Characters present in the dataset: {''.join(sorted(chars))}")
     print(
@@ -60,3 +60,11 @@ if __name__ == "__main__":
     print(
         f"Characters in the dataset but not allowed: {''.join(sorted(chars - set(my_chars)))}"
     )
+    # count number of not allowed characters
+
+    not_allowed_chars: dict[str, int] = {}
+    for char in dataset:
+        if char not in my_chars:
+            not_allowed_chars[char] = not_allowed_chars.get(char, 0) + 1
+    print(f"Number of not allowed characters: {len(not_allowed_chars)}")
+    print(f"Not allowed characters: {not_allowed_chars}")
